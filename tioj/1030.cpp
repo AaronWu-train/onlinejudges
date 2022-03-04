6498d91ddbe32f3714aeb4cc8cae68b04e4dafbb
AaronWu-train <aaronwu0218@gmail.com>
2022-03-04 10:47:08 +0800

    Create 1030.cpp

---
+++ tioj/1030.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+#define ll long long int
+#define debug(x) cerr<<#x<<" = "<<(x)<<endl
+#define _ ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+
+
+int main() {_
+    
+    int n;
+    cin >> n;
+    while (n!=0) {
+        ll ans = 2;
+        vector<ll>arr;
+        ll maxn = LLONG_MIN;
+        while (n--) {
+            ll temp;
+            cin >> temp;
+            ans+=temp;
+            maxn = max(maxn, temp);
+            arr.push_back(temp);
+        }
+        ans+=maxn-1;
+        cout << ans <<endl;
+        cin >> n;
+    }
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
