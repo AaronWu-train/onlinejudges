ec8b140734426b3f821d0e8346127d6da46e01cd
AaronWu-train <aaronwu0218@gmail.com>
2022-03-04 11:46:56 +0800

    20220304-1

---
+++ FN && Lemonline Judge/4460.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <climits>
+#define ll long long int
+#define debug(x) cerr<<#x<<" = "<<(x)<<endl
+#define _ ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+void pow(ll &a[3][3] ){
+    
+}
+/*
+ dp[i][0] = dp[i-1][0]*4 + dp[i-1][1]*1 + dp[i-1][2]*0  //兩偶
+ dp[i][1] = dp[i-1][0]*2 + dp[i-1][1]*4 + dp[i-1][2]*1  //一奇數一偶數
+ dp[i][2] = dp[i-1][0]*0 + dp[i-1][1]*1 + dp[i-1][2]*4  //兩奇數
+                 [[a],
+                  [b],
+                  [c]]
+        [[4,1,0],
+         [2,4,2],
+         [0,1,4]]
+ 
+ */
+
+
+int main() {_
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
